779b2ff24d0a474508c486c2b0430ab8bb555118
FlyingMedusa <sleboda.m98@gmail.com>
2019-12-30 22:22:22 +0100

    homeworks

--- Homeworks/024_recursion_factorial.py
+++ Homeworks/024_recursion_factorial.py
@@ -1,9 +1,14 @@
 def fac(x):
+    operation_list.append(str(x))
     if x == 1:
         return 1
     else:
         return x * fac(x-1)
 
 
+operation_list = []
 nat = int(input("Please enter a natural number:\t"))
-print(str(nat) + "! =", fac(nat))
+result = fac(nat)
+whole_operation = " * ".join(operation_list)
+print(str(nat) + "! =", whole_operation, "=", result)
+
