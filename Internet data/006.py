1b43b4fbb972e534eec667936a6c15910473180c
FlyingMedusa <sleboda.m98@gmail.com>
2020-03-29 16:52:05 +0200

    parse html to get just container with links

---
+++ Internet data/006.py
@@ -0,0 +1,12 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+print('Getting staff urls...')
+
+staff_url = 'http://wa.amu.edu.pl/wa/en/staff_list'
+response = urllib.request.urlopen(staff_url)
+data = response.read()
+doc = BeautifulSoup(data, 'html.parser')
+
+staff_content = doc.find(id='tresc_wlasciwa')
+print(staff_content)
