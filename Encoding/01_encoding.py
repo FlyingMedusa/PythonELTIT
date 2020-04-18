453c5ccca725a0ea51885b3ac640655973c4600b
FlyingMedusa <sleboda.m98@gmail.com>
2020-04-18 22:55:02 +0200

    Adding .gitignore

---
+++ Encoding/01_encoding.py
@@ -0,0 +1,13 @@
+
+
+f = open('r1251.txt', 'rb')
+r = f.read()
+r = r.decode('1251') # windows-1251 encoding
+f.close()
+print('Some Russian: ', r)
+
+f = open('cb5.txt', 'rb')
+c = f.read()
+c = c.decode('big5')
+f.close()
+print('Some Chinese: ', c)
